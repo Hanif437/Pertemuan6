7d31ba3e8a7a96c8a6996e02760d31d5e00e387f
Putra Hanif Ammarta <58537113+Hanif437@users.noreply.github.com>
2023-03-24 01:51:36 +0700

    merubah kode

--- app/src/main/java/id/ac/unpas/pertemuan6/model/JokiGenshin.kt
+++ app/src/main/java/id/ac/unpas/pertemuan6/model/JokiGenshin.kt
@@ -1,6 +1,11 @@
 package id.ac.unpas.pertemuan6.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class JokiGenshin(
+    @PrimaryKey val id : String,
     val tanggal: String,
     val uid : String,
     val nickname : String,
